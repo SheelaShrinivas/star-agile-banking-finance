3c497b6d97516a7f5c10adae4b60dc72d28f8ec2
sheela <sheela@gmail.com>
2024-07-12 16:08:50 +0530

    update bank-app

--- bank-app.tf
+++ bank-app.tf
@@ -1,138 +1,148 @@
-    }
-  }
-}
-
-# Configure the AWS provider
 provider "aws" {
-  region = "ap-south-1"
-}
-# Creating a VPC
-resource "aws_vpc" "proj-vpc" {
- cidr_block = "10.0.0.0/16"
+  region     = "ap-south-1"
+  access_key = "AKIAVRUVPMRC2S2QUFNG"
+  secret_key = "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"
 }
 
-# Create an Internet Gateway
-resource "aws_internet_gateway" "proj-ig" {
- vpc_id = aws_vpc.proj-vpc.id
- tags = {
- Name = "gateway1"
- }
+# Create VPC
+
+resource "aws_vpc" "myvpc1" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "myvpc1"
+  }
 }
 
-# Setting up the route table
-resource "aws_route_table" "proj-rt" {
- vpc_id = aws_vpc.proj-vpc.id
- route {
- # pointing to the internet
- cidr_block = "0.0.0.0/0"
- gateway_id = aws_internet_gateway.proj-ig.id
- }
- route {
- ipv6_cidr_block = "::/0"
- gateway_id = aws_internet_gateway.proj-ig.id
- }
- tags = {
- Name = "rt1"
- }
+# Create Subnet 
+
+resource "aws_subnet" "mysubnet1" {
+  vpc_id     = aws_vpc.myvpc1.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "mysubnet1"
+  }
 }
 
-# Setting up the subnet
-resource "aws_subnet" "proj-subnet" {
- vpc_id = aws_vpc.proj-vpc.id
- cidr_block = "10.0.1.0/24"
- availability_zone = "ap-south-1b"
- tags = {
- Name = "subnet1"
- }
+# Internet Gateway
+
+resource "aws_internet_gateway" "mygw1" {
+  vpc_id = aws_vpc.myvpc1.id
+
+  tags = {
+    Name = "mygw1"
+  }
 }
 
-# Associating the subnet with the route table
-resource "aws_route_table_association" "proj-rt-sub-assoc" {
-subnet_id = aws_subnet.proj-subnet.id
-route_table_id = aws_route_table.proj-rt.id
-}
+# Route Table
 
-# Creating a Security Group
-resource "aws_security_group" "proj-sg" {
- name = "proj-sg"
- description = "Enable web traffic for the project"
- vpc_id = aws_vpc.proj-vpc.id
- ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+resource "aws_route_table" "myrt1" {
+  vpc_id = aws_vpc.myvpc1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.mygw1.id
   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "myrt1"
   }
- ingress {
- description = "HTTPS traffic"
- from_port = 443
- to_port = 443
- protocol = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
- }
- ingress {
- description = "HTTP traffic"
- from_port = 0
- to_port = 65000
- protocol = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
- }
- ingress {
- description = "Allow port 80 inbound"
- from_port   = 80
- to_port     = 80
- protocol    = "tcp"
- cidr_blocks = ["0.0.0.0/0"]
-  }
- egress {
- from_port = 0
- to_port = 0
- protocol = "-1"
- cidr_blocks = ["0.0.0.0/0"]
- ipv6_cidr_blocks = ["::/0"]
- }
- tags = {
- Name = "proj-sg1"
- }
 }
 
-# Creating a new network interface
-resource "aws_network_interface" "proj-ni" {
- subnet_id = aws_subnet.proj-subnet.id
- private_ips = ["10.0.1.10"]
- security_groups = [aws_security_group.proj-sg.id]
+# Route Table Association
+
+resource "aws_route_table_association" "myrta1" {
+  subnet_id      = aws_subnet.mysubnet1.id
+  route_table_id = aws_route_table.myrt1.id
+}
+
+# Security Groups
+
+resource "aws_security_group" "mysg1" {
+  name        = "mysg1"
+  description = "Allow inbound traffic"
+  vpc_id      = aws_vpc.myvpc1.id
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+   description = "HTTPS traffic"
+   from_port = 443
+   to_port = 443
+   protocol = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+   description = "HTTP traffic"
+   from_port = 0
+   to_port = 65000
+   protocol = "tcp"
+   cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "mysg1"
+  }
+}
+
+#Creating a new network interface
+resource "aws_network_interface" "myni1" {
+ subnet_id = ws_subnet.mysubnet1.id
+ private_ips = ["10.0.0.10"]
+ security_groups = [aws_security_group.mysg1.id]
 }
 
 # Attaching an elastic IP to the network interface
-resource "aws_eip" "proj-eip" {
+resource "aws_eip" "myeip1" {
  vpc = true
- network_interface = aws_network_interface.proj-ni.id
- associate_with_private_ip = "10.0.1.10"
+ network_interface = aws_network_interface.myni1.id
+ associate_with_private_ip = "10.0.0.1"
 }
 
+# Create Instance
 
-# Creating an ubuntu EC2 instance
-resource "aws_instance" "Prod-Server" {
- ami = "ami-0ec0e125bb6c6e8ec"
- instance_type = "t2.micro"
- availability_zone = "ap-south-1"
- key_name = "ss"
- network_interface {
+resource "aws_instance" "test server" {
+  ami           = "ami-04f8d7ed2f1a54b14"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  subnet_id = aws_subnet.mysubnet1.id
+  vpc_security_group_ids = [aws_security_group.mysg1.id]
+  key_name = "ss"
+  network_interface {
  device_index = 0
- network_interface_id = aws_network_interface.proj-ni.id
+ network_interface_id = ws_network_interface.myni1.id
  }
  user_data  = <<-EOF
  #!/bin/bash
      sudo apt-get update -y
  EOF
- tags = {
- Name = "Prod-Server"
- }
+
+  tags = {
+    Name = "TF-PROJECT"
+  }
 }
